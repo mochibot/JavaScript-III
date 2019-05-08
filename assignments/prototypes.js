a09f9abbffb23552267104e70dcaa2dbb92bdd37
mochibot <amusaur@gmail.com>
2019-05-08 13:31:34 -0700

    Project meets MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,13 +15,33 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(gameAttri) {
+  this.createdAt = gameAttri.createdAt;
+  this.name = gameAttri.name;
+  this.dimensions = gameAttri.dimensions;
+}
 
+
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game`;
+}
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(charStatsAttri) {
+  GameObject.call(this, charStatsAttri);
+  this.healthPoints = charStatsAttri.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage.`
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +52,18 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(humanoidAttri) {
+   CharacterStats.call(this, humanoidAttri);
+   this.team = humanoidAttri.team;
+   this.weapons = humanoidAttri.weapons;
+   this.language = humanoidAttri.language;
+ }
+
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+ Humanoid.prototype.greet = function () {
+   return `${this.name} offers a greeting in ${this.language}.`
+ }
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +72,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +133,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit binding: 
+     "this" refers to the object to the left of the dot, when the function is invoked by a preceding dot
+* 2. Explicit binding
+     If there is no dot preceding the function, this is defined when the .call(), .bind(), or .apply() methods is invoked
+* 3. New binding
+     When a function is called with "new" keyword, "this" is referencing the new object that is being created
+  4. Global/window binding
+     Occurs in the global context where the value of "this" is the window/console object (when none of the other 3 principles apply).
+
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +17,59 @@
 // Principle 1
 
 // code example for Window Binding
+function saySomething() {
+  console.log(`There are ${this.num} Pokemons in my house`);
+}
+
+window.num = 50;
+saySomething();   
 
 // Principle 2
 
 // code example for Implicit Binding
+let animal = {
+  name: "guinea pig",
+  sound: "wheek",
+  speak: function() {
+    console.log(`${this.sound} I am a ${this.name}`);
+  }
+}
+
+animal.speak();
 
 // Principle 3
 
 // code example for New Binding
+function User(name, password) {
+  this.name = name,
+  this.password = password,
+  this.access = function () {
+    if (this.password.length > 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const user1 = new User('Pikachu', '123490');
+console.log(user1.name);
+console.log(user1.password);
+console.log(user1.access());
 
 // Principle 4
 
 // code example for Explicit Binding
+const myObj = {
+  name: "Penny",
+  adjective: "weird",
+  verb: "eat"
+}
+
+const citiesLivedIn = ['New York City', 'San Diego'];
+
+function aboutMe(city1, city2){
+  return `Hi! My name is ${this.name}. I am ${this.adjective} and I love to ${this.verb}. I lived in ${city1} and ${city2}.`
+}
+
+console.log(aboutMe.call(myObj, ...citiesLivedIn));
